797953178d9073ff6ddc3964c8835c2e0360884e
unknown <bddavis2011@gmail.com>
2022-07-19 17:32:14 -0500

    first save

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'visual stimulation';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cinema.indiana.edu/film_images/princess-mononoke.jpg';
+  image3 = 'https://cdna.artstation.com/p/assets/images/images/023/950/460/large/giovani-kososki-coffey-close-01.jpg?1580843860&dl=1';
 
   constructor() { }
 
